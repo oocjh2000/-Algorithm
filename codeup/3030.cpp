d12acc66dc8bb43af8afbfbebae613f3ba30fd28
DESKTOP-27VVUBG\chats <oocjh200@gmail.com>
2018-06-15 19:38:05 +0900

    .

---
+++ codeup/3030.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	int ArraySize;
+	int Index;
+
+
+	cin >> ArraySize;
+
+
+	
+
+
+
+
+	return 0;
+}
